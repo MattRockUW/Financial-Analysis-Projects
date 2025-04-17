65141461db6fc428aa56fdf4172be21d357c91f1
Matt Rock <mrock@uwhealth.org>
2025-04-17 10:12:48 -0500

    first draft sent to Brian
    added relevant names, still missing some odd cases due to a join that'll be investigated

--- Templates/Surgical Logs.sql
+++ Templates/Surgical Logs.sql
@@ -4,11 +4,17 @@ select
 olc.log_id, 
 olc.surgery_date, 
 olc.room_id,
-olc.loc_id,
+A.PRIN_GL_BUILDING,
 locat.location_nm,
 olc.num_of_panels,
 eocap.or_proc_id,
 eopc.proc_name, 
+--eflb.scheduled_in_or_dttm,
+--eflb.scheduled_out_or_dttm,
+--eflb.in_or_dttm,
+--eflb.out_or_dttm,
+eflb.minutes_scheduled_in_or,
+eflb.minutes_in_or, 
 epoalc.pat_enc_csn_id,
 epoalc.pat_id, 
 epoalc.or_link_csn,
@@ -16,12 +22,13 @@ epoalc.or_link_inp_id,
 ADMIT_DEPARTMENT_ID,
 ADMIT_DEPARTMENT,
 PRIN_CPT_CODE,
+eflb.inpatient_yn,
 PRIN_ICD10_PROCEDURE_CODE,
 PRIN_ICD10_DIAGNOSIS_CODE,
 ADMIT_ICD10_DIAGNOSIS_CODE,
-ADMIT_PROVIDER_ID,
-ATTEND_PROVIDER_ID,
-PRINCIPAL_SURGEON_ID,
+ADMIT.name as AdmitProviderName,
+ATTEND.name as AttendingProviderName,
+surg.name as SurgeonName,
 OR_CASES,
 MSDRG_CODE,
 MSDRG_CODE_GROUP,
@@ -31,6 +38,7 @@ APRDRG_CODE,
 APRDRG_ROM,
 APRDRG_SOI,
 MSMDC,
+A.INS_PLAN_1_GL_PAYOR_GROUP_1,
 INSURANCE_PLAN_1,
 ADMIT_DT,
 DISCHARGE_DT,
@@ -43,28 +51,15 @@ COALESCE(A.FIXED_INDIRECT_COST, 0) + COALESCE(M_PB.FIXED_INDIRECT_COST, 0) FIXED
 COALESCE(A.ESTIMATED_REIMBURSEMENT, 0)  + COALESCE(M_PB.ESTIMATED_REIMBURSEMENT , 0) 
 - COALESCE(A.DIRECT_COST, 0)  - COALESCE(M_PB.DIRECT_COST, 0) 
 - COALESCE(A.VAR_INDIRECT_COST, 0)  - COALESCE(M_PB.VAR_INDIRECT_COST, 0)  CONTR_MARGIN,
---Contribution Margin Perc
---(COALESCE(A.ESTIMATED_REIMBURSEMENT, 0)  + COALESCE(M_PB.ESTIMATED_REIMBURSEMENT, 0) 
---- COALESCE(A.DIRECT_COST, 0)  - COALESCE(M_PB.DIRECT_COST, 0) 
---- COALESCE(A.VAR_INDIRECT_COST, 0)  - COALESCE(M_PB.VAR_INDIRECT_COST, 0) )/
---(COALESCE(A.ESTIMATED_REIMBURSEMENT, 0)  + COALESCE(M_PB.ESTIMATED_REIMBURSEMENT, 0) ) CONTR_MARGIN_PERC,
---Operating Margin
 COALESCE(A.ESTIMATED_REIMBURSEMENT, 0)  + COALESCE(M_PB.ESTIMATED_REIMBURSEMENT, 0) 
 - COALESCE(A.DIRECT_COST, 0)  - COALESCE(M_PB.DIRECT_COST, 0) 
 - COALESCE(A.VAR_INDIRECT_COST, 0)  - COALESCE(M_PB.VAR_INDIRECT_COST, 0) 
 - COALESCE(A.FIXED_INDIRECT_COST, 0)  - COALESCE(M_PB.FIXED_INDIRECT_COST, 0) OP_MARGIN
---,
---Operating Margin Perc
---(COALESCE(A.ESTIMATED_REIMBURSEMENT, 0)  + COALESCE(M_PB.ESTIMATED_REIMBURSEMENT, 0) 
---- COALESCE(A.DIRECT_COST, 0)  - COALESCE(M_PB.DIRECT_COST, 0) 
---- COALESCE(A.VAR_INDIRECT_COST, 0)  - COALESCE(M_PB.VAR_INDIRECT_COST, 0) 
---- COALESCE(A.FIXED_INDIRECT_COST, 0)  - COALESCE(M_PB.FIXED_INDIRECT_COST, 0) )/
---(COALESCE(A.ESTIMATED_REIMBURSEMENT, 0)  + COALESCE(M_PB.ESTIMATED_REIMBURSEMENT, 0) ) OP_MARGIN_PERC
-
 from [Source_UWHealth].EPIC_OR_LOG_CUR olc
 join [Source_UWHealth].EPIC_OR_case_ALL_PROC_CUR eocap on olc.log_id = eocap.or_Case_id
 join [Source_UWHealth].EPIC_OR_PROC_CUR eopc on eocap.or_proc_id = eopc.or_proc_id
 join [Source_UWHealth].EPIC_PAT_OR_ADM_LINK_CUR epoalc on epoalc.log_id = eocap.or_case_id  
+join [Source_UWHealth].EPIC_F_LOG_BASED_CUR eflb on eflb.log_id = olc.log_id
 join [Modeled_UWHealth].DIM_LOCATION_CUR locat on locat.loc_id = olc.loc_id
 join [Mart_UWHealth].[STRATA_COST_ACCOUNT_HB] A on a.pat_enc_csn_id=epoalc.or_link_csn
 LEFT JOIN --Joining on Matched PB
@@ -100,7 +95,17 @@ HAVING SUM([CHARGE_AMOUNT]) > 0  --Must have positive charges in OR
 ) B
 WHERE
 RANK1 = 1) PRIM_OR ON A.STRATA_ENCOUNTER_REC_NBR = PRIM_OR.STRATA_ENCOUNTER_REC_NBR
-where --eocap.or_proc_id = '339628' 
-olc.surgery_date >= '01/01/2024'
-and eopc.proc_name = 'ARTHROPLASTY, KNEE, TOTAL'
+join [source_uwhealth].epic_CLARITY_EMP_cur admit on admit.prov_id = ADMIT_PROVIDER_ID
+join [source_uwhealth].epic_CLARITY_EMP_cur ATTEND on ATTEND.prov_id = ATTEND_PROVIDER_ID
+join [source_uwhealth].epic_CLARITY_EMP_cur surg on surg.prov_id = PRINCIPAL_SURGEON_ID
+where eflb.primary_procedure_id in ('137952', '138672', '138816', '138960', '139104', '139392', '1728', '300027', '300031', '300036', '300043', '339042', '339044', '339558', '339626', '339628', '4838', '4900', '4925', '5104', '5120', '5142', '5150', '5151', '5368', '5379', '5442', '5462', '5510', '5519', '6066', '6071', '6072', '6367', '6446', '6448', '6450', '6725', '7060', '7069', '7220', '7269', '7270', '7275', '7365', '7366', '7367', '7368', '7378', '7596', '7635', '7670', '7677', '81936', '82080', '82224', '82368', '92448', '92592', '93744', '94032', '94176', '94320', '94464', '94752', '94896', '95040')
+and eflb.primary_procedure_id = eocap.or_proc_id 
+and olc.surgery_date >= '01/01/2024'
+and olc.loc_id not in ('110037000')
+--and olc.log_id = '1075438'
 order by surgery_date desc
+
+--select top 100 * from [Source_UWHealth].EPIC_F_LOG_BASED_CUR
+--where loc_id in (136130,101102,134593,133464,136932)
+--join [Mart_UWHealth].[STRATA_COST_ACCOUNT_HB] A on a.pat_enc_csn_id=epoalc.or_link_csn
+
